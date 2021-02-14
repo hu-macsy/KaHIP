29381bf6adeeb19ee97695902f4fdfab88b48d24
tzovas <tzovas.charilaos@hu-berlin.de>
2021-02-14 19:29:49 +0100

    template in interface.h

--- parallel/parallel_src/interface/parhip_interface.h
+++ parallel/parallel_src/interface/parhip_interface.h
@@ -7,12 +7,12 @@
 #ifndef PARHIP_INTERFACE
 #define PARHIP_INTERFACE
 #include <mpi.h>
-#ifdef __cplusplus
+//#ifdef __cplusplus
 
-extern "C"
-{
-#endif
-typedef unsigned long long idxtype;
+//extern "C"
+//{
+//#endif
+//typedef unsigned long long idxtype;
 
 const int ULTRAFASTMESH   = 0;
 const int FASTMESH        = 1;
@@ -21,12 +21,13 @@ const int ULTRAFASTSOCIAL = 3;
 const int FASTSOCIAL      = 4;
 const int ECOSOCIAL       = 5;
 
+template<typename idxtype>
 void ParHIPPartitionKWay(idxtype *vtxdist, idxtype *xadj, idxtype *adjncy, idxtype *vwgt, idxtype *adjwgt,
                          int *nparts, double* imbalance, bool suppress_output, int seed, int mode, int *edgecut, idxtype *part, 
                          MPI_Comm *comm);
-#ifdef __cplusplus
-}
-#endif
+//#ifdef __cplusplus
+//}
+//#endif
 
 
 #endif /* end of include guard: PARHIP_INTERFAVE */
